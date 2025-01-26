5f1055e44d086db9cdf28d8cb788422fffe30203
Danny Flores <dannyafloresc@gmail.com>
2025-01-25 22:22:31 -0600

    PascalCase estilo de escritura para nombrar componentesen React.

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const createButton = ({text}) => {
+const Button = ({text}) => {
   return (
     <button>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -15,8 +15,8 @@ const createButton = ({text}) => {
 
 root.render(
   <React.Fragment>
-    {createButton({ text: 'Hola mundo' })}
-    {createButton({ text: 'Hello world' })}
+    <Button text="Hola Mundo" />
+    <Button text="Hello World" />
   </React.Fragment>
   
 )
