05ffc7462b7dc7a4287cd2f6220139b1354e67b8
Danny Flores <dannyafloresc@gmail.com>
2025-01-28 22:24:58 -0600

    Buena practica: margin manejarlo donde se este renderizando o mostrando el componente. Utilizar un prop en un Div o un Section. Hacer a nivel de app (index.css) donde se hagan estos ajustes. Así se puede manejar de varias formas en diferentes partes del app.
    Un solo estilo para un componente que puede ser renderizado varias veces, lo cual simplifica un cambio, solo se hace en un estilo.

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -3,10 +3,10 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
   return (
-    <>
+    <section className="App">
         <TwitterFollowCard userName="freecodecamp" name="Free Code Camp"/>
         <TwitterFollowCard userName="realmadrid" name="Real Madrid"/>
         <TwitterFollowCard userName="fcbarcelona" name="FC Barcelona"/>
-    </>
+    </section>
   )
 }
