e806ac3b0c388d81720b3dea8f16637af0cb688b
Danny Flores <dannyafloresc@gmail.com>
2025-01-28 22:07:09 -0600

    Crear componente reutilizable y props dinámicas.

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
-export function App() {
+export function App () {
   return (
-    <article className='tw-followCard'>
-        <header className='tw-followCard-header'>
-            <img
-                className='tw-followCard-avatar'
-                alt="El avatar de Mario"
-                src="https://unavatar.io/youtube/freecodecamp"></img>
-            <div className="tw-followCard-info">
-                <strong>Free Code Camp</strong>
-                <span
-                className='tw-followCard-infoUserName'>@freecodecamp</span>
-            </div>
-        </header>
-
-        <aside>
-            <button className='tw-followCard-button'>
-                Seguir
-            </button>
-        </aside>
-    </article>
+    <>
+        <TwitterFollowCard userName="freecodecamp" name="Free Code Camp"/>
+        <TwitterFollowCard userName="realmadrid" name="Real Madrid"/>
+        <TwitterFollowCard userName="fcbarcelona" name="FC Barcelona"/>
+    </>
   )
 }
