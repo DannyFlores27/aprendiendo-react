cdbd436dac02670ea50171d964c0bb98a83ca7ec
Danny Flores <dannyafloresc@gmail.com>
2025-02-09 16:14:14 -0600

    Pasar una función como Prop.
    Cambiando el estado del turno.

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,14 +1,66 @@
+import React, { useState } from 'react'
+
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard()
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
+  /* const board = Array(9).fill(null) */
+  const [board, setBoard] = useState(Array(9).fill(null))
+  /* const [board, setBoard] = useState(
+    ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
+  ) */
 
-  return <h1>Tic Tac Toe Game</h1>
+  const [turn, setTurn] = useState(TURNS.X)
 
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe Game</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
 }
 
 export default App
